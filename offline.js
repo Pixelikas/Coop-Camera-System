46b60517b1f584e4fa937b75220aa476082c8c20
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2023-12-06 22:29:32 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1701912465,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/player1-sheet0.png",
+		"images/player1-sheet1.png",
+		"images/player1-sheet2.png",
+		"images/player2-sheet0.png",
+		"images/player2-sheet1.png",
+		"images/player2-sheet2.png",
+		"images/cameraobject-sheet0.png",
+		"images/invisiblewall-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
